7f1e8121ef09a9f4105106aa55ba17cdfb433ac5
Edgardlt <edgardlt34@gmail.com>
2018-10-30 17:33:35 -0600

    Connection to mysql is ready

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,9 +1,20 @@
 var mysql = require("mysql");
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "",
-    database: "bamazon"
+let dataKeys = require("./keys.js");
+
+var connection = mysql.createConnection(dataKeys.mySql);
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
   });
+
+  function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+  }
