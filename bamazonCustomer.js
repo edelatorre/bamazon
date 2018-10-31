e06596d87f15b8a7d40467d73e82360930901443
Edgardlt <edgardlt34@gmail.com>
2018-10-31 16:16:43 -0600

    Advancing on the using of the DB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -32,8 +32,30 @@ connection.connect(function(err) {
     
         ]).then(function(purchase) {
             console.log(`You want to purchase ${purchase.units} units of the product ID = ${purchase.productId}`);
+            checkStock(purchase.productId);
         });
-      connection.end();
+      //connection.end();
     });
   };
 
+
+  function checkStock(IdProd) {
+    console.log("Checking stock of the product...\n");
+    var query = connection.query("SELECT stock_quantity FROM products where ?",
+    [
+        {
+            item_id: IdProd
+        }
+    ]
+    , function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.log(res);
+      var arreglo = Object.values(res[0])
+      var stock = arreglo[0]
+      console.log(stock)
+      console.log(Object.values(res[0])[0])
+      connection.end();
+    });
+    console.log(query.sql);
+  }
